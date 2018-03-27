831a3a4c04a96088d5a9867b2f23cf903549a841
RK <rafal.klewek@pollub.edu.pl>
2018-03-28 00:10:14 +0200

    Posiada wymagane funkcjonalności, kod do poprawy

--- app/src/main/java/com/example/div/sqlite_test/EditionActivity.java
+++ app/src/main/java/com/example/div/sqlite_test/EditionActivity.java
@@ -12,6 +12,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import static android.support.v4.util.PatternsCompat.WEB_URL;
+
+
 public class EditionActivity extends AppCompatActivity {
     private EditText producerEditText;
     private EditText modelEditText;
@@ -20,6 +23,10 @@ public class EditionActivity extends AppCompatActivity {
     private Button wwwButton;
     private Button cancelButton;
     private Button saveButton;
+    private final static boolean ACTION_ADD = true;
+    private final static boolean ACTION_UPDATE = false;
+    private boolean action;
+    int rowId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +36,12 @@ public class EditionActivity extends AppCompatActivity {
 
 
         try {
-            int rowId = getIntent().getExtras().getInt("ROW_ID");
+            rowId = getIntent().getExtras().getInt("ROW_ID");
             fillEditsFromDatabase(rowId);
-        }
-        catch (NullPointerException e) {
+            action = ACTION_UPDATE;
+        } catch (NullPointerException e) {
             Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+            action = ACTION_ADD;
         }
 
         addButtonListeners();
@@ -80,9 +88,18 @@ public class EditionActivity extends AppCompatActivity {
         wwwButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //if url == OK
-                //else error message
-                Intent wwwIntent = new Intent("android.intent.action.VIEW", Uri.parse("https://www.google.pl"));
+                String[] projection = {MyDBHelper.URL_COLUMN};
+                Cursor cursor = getContentResolver().query(
+                        ContentUris.withAppendedId(MyProvider.CONTENT_URI, rowId),
+                        projection,
+                        null,
+                        null,
+                        null
+                );
+                cursor.moveToFirst();
+                String url = cursor.getString(cursor.getColumnIndex(MyDBHelper.URL_COLUMN));
+                //Toast.makeText(EditionActivity.this, url, Toast.LENGTH_SHORT).show();
+                Intent wwwIntent = new Intent("android.intent.action.VIEW", Uri.parse("https://" + url));
                 startActivity(wwwIntent);
             }
         });
@@ -101,25 +118,63 @@ public class EditionActivity extends AppCompatActivity {
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //validate
-                //add/update
-                addValue();
+                boolean fieldsOk = validateFields();
+
+                if (fieldsOk) {
+                    modifyDatabase();
+                    finish();
+                }
             }
         });
     }
 
-    private void addValue() {
+    private boolean validateFields() {
+        String producerText = getStringFromEdit(producerEditText);
+        String modelText = getStringFromEdit(modelEditText);
+        String versionText = getStringFromEdit(versionEditText);
+        String wwwText = getStringFromEdit(wwwEditText);
+        boolean resultOfValidation = true;
+
+        if (!producerText.matches("[A-Z][a-z]{0,15}")) {
+            producerEditText.setError("Wypełnij to pole (zacznij z dużej litery)");
+            resultOfValidation = false;
+        }
+        if (!modelText.matches("[A-Z][A-Za-z0-9]{0,15}")) {
+            modelEditText.setError("Wypełnij to pole (zacznij z dużej litery)");
+            resultOfValidation = false;
+        }
+        if (!versionText.matches("[0-9]{1,3}([.][0-9]{1,3}){0,2}")) {
+            versionEditText.setError("Wypełnij to pole (np. 6.0.3)");
+            resultOfValidation = false;
+        }
+        if(!wwwText.matches("^(http:\\/\\/|https:\\/\\/)?(www.)?([a-zA-Z0-9]+).[a-zA-Z0-9]*.[a-z]{3}.?([a-z]+)?$")) {
+            wwwEditText.setError("Podaj poprawny adres WWW");
+            resultOfValidation = false;
+        }
+
+        return resultOfValidation;
+    }
+
+    private void modifyDatabase() {
         ContentValues values = new ContentValues();
-        producerEditText = findViewById(R.id.producerEditText);
-        modelEditText = findViewById(R.id.modelEditText);
-        versionEditText = findViewById(R.id.versionEditText);
-        wwwEditText = findViewById(R.id.wwwEditText);
+        values.put(MyDBHelper.PRODUCER_COLUMN, getStringFromEdit(producerEditText));
+        values.put(MyDBHelper.MODEL_COLUMN, getStringFromEdit(modelEditText));
+        values.put(MyDBHelper.VERSION_COLUMN, getStringFromEdit(versionEditText));
+        values.put(MyDBHelper.URL_COLUMN, getStringFromEdit(wwwEditText));
 
-        values.put(MyDBHelper.PRODUCER_COLUMN, producerEditText.getText().toString());
-        values.put(MyDBHelper.MODEL_COLUMN, modelEditText.getText().toString());
-        values.put(MyDBHelper.VERSION_COLUMN, versionEditText.getText().toString());
-        values.put(MyDBHelper.URL_COLUMN, wwwEditText.getText().toString());
+        if (action == ACTION_ADD) {
+            getContentResolver().insert(MyProvider.CONTENT_URI, values);
+        } else if (action == ACTION_UPDATE) {
+            getContentResolver().update(
+                    ContentUris.withAppendedId(MyProvider.CONTENT_URI, rowId),
+                    values,
+                    null,
+                    null
+            );
+        }
+    }
 
-        Uri uriOfNew = getContentResolver().insert(MyProvider.CONTENT_URI, values);
+    private String getStringFromEdit(EditText eT) {
+        return eT.getText().toString();
     }
 }
